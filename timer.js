834f8e02f309aac2462dd9fc40c4ea0e7acd68ad
Artemis Papanikolaou <artemispapanik@gmail.com>
2021-02-19 11:37:08 +0000

    Start and pause buttons have a basic function

--- timer.js
+++ timer.js
@@ -1,28 +1,57 @@
-let intervalTimer = setInterval(timerHandle, 1000);
-
-
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+
 let displaySeconds;
 let displayMinutes;
 let displayHours;
 
+let timer = 0;
+
+// function start() {
+//     $(".start-button").click(function () {
+//         timer = setInterval(timerHandle, 1000);
+//         $(".start-button").off("click");
+//     });
+// }
+
+$(".start-button").click(function () {
+    timer = setInterval(timerHandle, 1000);
+    $(".start-button").off("click");
+});
+
+$(".pause-button").click(function () {
+    timer = clearInterval(timer);
+    // timer = setInterval(timerHandle, 1000);
+    // $(".pause-button").off("click");
+
+    // add an if/else = > if the pause button is clicked then add the class paused and clearInterval, and when clicked again it should start the setInterval
+});
+
+$(".stop-button").click(function () {
+    timer = clearInterval(timer);
+    // timer = setInterval(timerHandle, 1000);
+    $(".stop-button").off("click");
+});
+
+
+
+
 
 function timerHandle() {
- seconds++;
- if (seconds === 60) {
-     seconds = 0;
-     minutes++;
- }
+    seconds++;
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
 
- if (minutes === 60) {
-    minutes = 0;
-    hours++;
- }
+    if (minutes === 60) {
+        minutes = 0;
+        hours++;
+    }
 
-timerDisplay();
-console.log(hours + " . " + minutes + "." +seconds)
+    timerDisplay();
+    // console.log(hours + "." + minutes + "." +seconds)
 }
 
 function timerDisplay(){
